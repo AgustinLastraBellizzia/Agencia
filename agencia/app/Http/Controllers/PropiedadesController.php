0f5d9413565e7de7e156146e07d6f5d8d14653e4
AgustinLastraBellizzia <lastrabellizzia@gmail.com>
2021-03-15 14:00:55 -0600

    Sub
    Sub

---
+++ agencia/app/Http/Controllers/PropiedadesController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Propiedad;
+
+class PropiedadesController extends Controller
+{
+    function createPropiedad(Request $request){
+        //return view('welcome');
+    $propiedad = new Propiedad;
+    $propiedad->titulo = "Departamentos torre empresarial";
+    $propiedad->descripcion = "Hemosos departamentos";
+    $propiedad->precio = 123456;
+    $propiedad->localizacion = "Villahermosa";
+    $propiedad->area = "150m2";
+    $propiedad->cuartos = 2;
+    $propiedad->banios = 2;
+    $propiedad->garages= 2;
+    $propiedad->categoria_id = 1;
+
+    if($propiedad->save()){
+        $data = array(
+            'status' => 'success',
+            'message' => 'Propiedad creada',
+            'propiedad' => $propiedad
+            
+        );
+    }else{
+        $data = array(
+            'status' => 'error',
+            'message' => 'Propiedad no fue creada',
+        );
+    }
+
+    return response()->json($data);
+    }
+
+    function listPropiedades(Request $request){
+        $data = Propiedad::paginate(4);
+        return response()->json($data);
+    }
+}
