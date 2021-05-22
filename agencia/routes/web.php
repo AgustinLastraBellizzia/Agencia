ce5e7751778eaaa9c46e733e4a4eade5214a9a93
AgustinLastraBellizzia <lastrabellizzia@gmail.com>
2021-05-22 15:55:56 -0500

    Panel de ADMIN

--- agencia/routes/web.php
+++ agencia/routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Categoria;
 use App\Http\Controllers\PropiedadesController;
 use App\Http\Controllers\ContactoContoller;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\AgentesController;
 use App\Propiedad;
 
 /*
@@ -29,3 +31,16 @@ Route::get('/crearpropiedad', [PropiedadesController::class,'createPropiedad']);
 Route::get('/listapropiedades', [PropiedadesController::class,'listPropiedades']);
 Route::get('/propiedad/{slug}/detalle', [PropiedadesController::class,'detallePropiedad'])->name('detallepropiedad');
 Route::post('/guardar/contacto', [ContactoContoller::class,'saveContacto'])->name('save.contacto');
+
+
+Route::get('/api/propiedades', function(){
+    $propiedades = Propiedad::orderBy('id','DESC')->with('Categoria')->paginate(5);
+    return response()->json($propiedades);
+})->name('home');
+
+//administracion de las propiedades
+Route::prefix('admin')->group(function (){
+    Route::get('/', [UserController::class, 'index'])->name('login');
+    Route::get('/dashboard', [UserController::class, 'dashboard'])->name('dashboard');
+    Route::get('/agentes', [AgentesController::class, 'listaAgentes'])->name('lista.agentes');
+});
